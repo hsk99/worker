ebaf6fc5041be5f35ff2b74cef1378e50c89f26f
hsk99 <3024186605@qq.com>
2020-07-28 17:27:30 +0800

    更新

---
+++ support/bootstrap/Push.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace support\bootstrap;
+
+use GatewayClient\Gateway;
+
+class Push
+{
+    /**
+     * GatewayWorker 注册地址
+     * @var string
+     */
+    public static $register = '';
+
+    /**
+     * 内部子进程地址
+     * @var string
+     */
+    public static $internal = '';
+
+    /**
+     * @method 执行推送
+     *
+     * @param  string $data [description]
+     * @return [type]       [description]
+     */
+    public static function send ($data = '', $type = '', $value = '')
+    {
+        if (!empty(self::$register)) {
+            self::register($data, $type, $value);
+        }
+
+        if (!empty(self::$internal)) {
+            self::internal($data);
+        }
+    }
+
+    /**
+     * @method 使用 GatewayClient 推送
+     *
+     * @param  string   $data  [description]
+     * @param  string   $type  [description]
+     * @param  string   $value [description]
+     * @return [type]          [description]
+     */
+    protected static function register ($data = '', $type = '', $value = '')
+    {
+        try {
+            Gateway::$registerAddress  = self::$register;
+
+            switch ($type) {
+                case 'uid':
+                    Gateway::sendToUid($value, $data);
+                    break;
+                case 'client':
+                    Gateway::sendToClient($value, $data);
+                    break;
+                case 'group':
+                    Gateway::sendToGroup($value, $data);
+                    break;
+                case 'all':
+                    Gateway::sendToAll($data);
+                    break;
+            }
+
+            return true;
+        } catch (Exception $e) {
+            return false;
+        }
+    }
+
+    /**
+     * @method 使用 内部子进程 执行推送
+     *
+     * @param  string   $data [description]
+     * @return [type]         [description]
+     */
+    protected static function internal ($data = '')
+    {
+        $client = @stream_socket_client(self::$internal, $errno, $errmsg);
+        if (!$client) {
+            return false;
+        } else {
+            fwrite($client, $data);
+            fclose($client);
+            return true;
+        }
+    }
+}
